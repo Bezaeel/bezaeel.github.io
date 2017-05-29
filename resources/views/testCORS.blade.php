862ffe7a1531d763ba6ef6fd9e80caf4aecba26c
Bezaeel <talabi.rotimi@hotmail.com>
2017-05-29 21:33:14 +0100

    emptied

--- resources/views/testCORS.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-
-<a href="#" onclick="makeCorsRequest(); return false;">test CORS</a>
-
-<script type="text/javascript" src="{{asset('assets/cors.js')}}"></script>
-@endsection
